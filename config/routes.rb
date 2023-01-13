88f1ee9febdeaab86fcc77550498ce4fd577a5aa
EgoisticHero3245 <ojnohognib@gmail.com>
2023-01-13 20:17:49 +0900

    出品するにアクセス

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'posts#index'
+  root to: 'items#index'
+
   resources :users, only: [:edit, :update]
-  resources :buys, only: [:index, :new]
+  resources :items, only: [:index, :new]
   
 end
