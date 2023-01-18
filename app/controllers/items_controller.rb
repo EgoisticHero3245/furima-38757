83422d33890de04a14ff59e1468bb237132742ac
EgoisticHero3245 <ojnohognib@gmail.com>
2023-01-18 16:38:59 +0900

    商品一覧表示機能-1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    # @items = Item.all
+    @items = Item.all
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        #  has_many :items
+         has_many :items
         #  has_many :buys
 
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
