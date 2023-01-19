4835eb6f59880bb1e14d2e8d7a14d61591b06849
EgoisticHero3245 <ojnohognib@gmail.com>
2023-01-19 15:52:02 +0900

    商品情報編集機能-1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path
+    else
+      render :edit
+      end
+  end
+
 
   private
 
