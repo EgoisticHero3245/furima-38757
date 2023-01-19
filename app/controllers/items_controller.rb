947b3bae616ca11088be8269f7dba4c47b0cd40b
EgoisticHero3245 <ojnohognib@gmail.com>
2023-01-19 16:47:27 +0900

    商品一覧表示機能-2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,10 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
   end
 
   def update
