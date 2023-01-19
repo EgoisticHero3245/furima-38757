1cf6fcd9c5e1273a20102fb3ef29d5d9dc1084f6
EgoisticHero3245 <ojnohognib@gmail.com>
2023-01-19 20:13:15 +0900

    商品一覧表示機能-3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,8 @@ class ItemsController < ApplicationController
   end
 
    def destroy
-    if @item.destroy
+    if @item.user_id == current_user.id
+      @item.destroy
       redirect_to root_path
     else
       redirect_to root_path
