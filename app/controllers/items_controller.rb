23f869425f9bc0976890b7ba4af48c24149fb68f
EgoisticHero3245 <ojnohognib@gmail.com>
2023-01-19 19:21:07 +0900

    商品削除機能-1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -39,6 +39,13 @@ class ItemsController < ApplicationController
       end
   end
 
+   def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+   end
 
   private
 
